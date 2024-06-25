e4154667c87bfd7e769f51ebc8988ba0c5f951eb
Sameer Shaik <sameer.kattubadi@gmail.com>
2024-06-25 23:24:52 +0530

    login

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/Navbar';
+
 import Footer from './components/Footer';
-import { BrowserRouter } from 'react-router-dom';
+
 import Home from './components/Home';
 
 function App() {
   return (
-    <BrowserRouter>
-    <div className="App">
-      {/* <Navbar /> */}
-      <Home />
-      <Footer />
-    </div>
-    </BrowserRouter>
+  
+      <div className="App">
+        <Home />
+        <Footer />
+        
+      </div>
+    
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Authentication from './components/Authentication';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Authentication/>} />
+      <Route path="/app" element={<App/>} />
+      
+
+    </Routes>
+     
+    </BrowserRouter>
 
   </React.StrictMode>
 );
