8cba03ddb0608126b31d17e20b759e784366bcd7
Sameer Shaik <sameer.kattubadi@gmail.com>
2024-06-27 01:10:37 +0530

    server design and folders setup

--- server/index.js
+++ server/index.js
@@ -1,12 +1,31 @@
 const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+require('dotenv').config()
 const app = express();
 
 
+
+
 // Middle Ware
+app.use(bodyParser.json());
+app.use(cors());
 
 
+
+//variables
+const PORT = process.env.PORT;
 
 // Routes
+app.get("/",(req, res) => {
+    res.sendFile(__dirname + "/files/index.html")
+})
 
+app.get("/doc",(req, res) => {
+    res.sendFile(__dirname + "/files/SkinAI_SRS.docx")
+})
 
 // Listen
+app.listen(PORT, (req, res) => {
+    console.log(`Server Is Running http://localhost:${PORT}`)
+})
