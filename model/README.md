084fe1745e8ef3bfddea1172d03e73c0110c0a67
Sameer Shaik <sameer.kattubadi@gmail.com>
2024-11-01 15:53:45 +0530

    fix: model trial development

---
+++ model/README.md
@@ -0,0 +1,7 @@
+## Flask Server
+
+- Flask is a python framework suitable for light weight applications.
+
+# How to Run the Server
+
+- python app.py
--- model/app.py
+++ model/app.py
@@ -1,16 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
+import model.gui as gui
+import io
 import datetime
-
-
 
 app = Flask(__name__)
 
 @app.get("/")
 def server_run():
     uptime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    return render_template("index.html",uptime=uptime)
+    return render_template("index.html", uptime=uptime)
 
-
+@app.get("/image/generate")
+def gen():
+    # Create a BytesIO stream and save the image to it
+    data = io.BytesIO()
+    image = gui.generateImage()
+    image.save(data, format="JPEG")
+    data.seek(0)  # Move to the beginning of the BytesIO stream
+    return send_file(data, mimetype="image/jpeg")
 
 if __name__ == "__main__":
     app.run(debug=True)
