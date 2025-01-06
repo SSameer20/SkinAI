8e74b4be725f3b8d6e8e0ae9c0568daee0b54ad8
Sameer Shaik <sameer.kattubadi@gmail.com>
2025-01-07 00:21:12 +0530

    fix: card layout

--- client/src/components/ui/Card.tsx
+++ client/src/components/ui/Card.tsx
@@ -1,3 +1,35 @@
+import { IconType } from "react-icons/lib";
+
+export function TeamCard({
+  key,
+  className,
+  name,
+  role,
+  image,
+}: {
+  key: number | string;
+  className?: string;
+  name: string;
+  role: string;
+  image: string;
+}) {
+  return (
+    <div
+      key={key}
+      className={
+        className +
+        " flex flex-row justify-around items-center bg-white p-6 rounded-lg shadow-md h-auto"
+      }
+    >
+      <img src={image} alt="user" className=" h-[100px] rounded-[50%]" />
+      <div className="h-auto">
+        <h3 className="text-xl font-semibold">{name}</h3>
+        <p className="text-blue-600 mb-3">{role}</p>
+      </div>
+    </div>
+  );
+}
+
 export function ValueCard({
   key,
   className,
