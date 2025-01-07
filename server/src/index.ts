b88985f4030086cf9517b065a1c9b401a7006ac5
Sameer Shaik <sameer.kattubadi@gmail.com>
2025-01-07 12:33:54 +0530

    fix: server ports forease deployment

--- server/src/index.ts
+++ server/src/index.ts
@@ -44,7 +44,7 @@ if (!mongoURI) {
 
 ConnectDB(mongoURI)
   .then((): void => {
-    const PORT = process.env.PORT || 8080;
+    const PORT = process.env.PORT || 5000;
     app.listen(PORT, () => {
       log.info(`Server is Running on http://localhost:${PORT}`);
     });
