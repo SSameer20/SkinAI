21158c09148aa6786d2c1fa0089f59bea269b95d
Sameer Shaik <sameer.kattubadi@gmail.com>
2025-05-30 23:17:51 +0530

    image classification model

--- ml/app.py
+++ ml/app.py
@@ -2,27 +2,50 @@ from flask import Flask, request, jsonify
 import tensorflow as tf
 import numpy as np
 from PIL import Image
-import matplotlib.pyplot as plt # type: ignore
-
-# Load the saved model
-model = tf.keras.models.load_model("./model/saved_models/model_v_0_1_3.keras")
-
-# Class names
-class_names = ['Acne and Rosacea Photos', 'Actinic Keratosis Basal Cell Carcinoma and other Malignant Lesions', 'Atopic Dermatitis Photos', 'Bullous Disease Photos', 'Cellulitis Impetigo and other Bacterial Infections', 'Eczema Photos', 'Exanthems and Drug Eruptions', 'Hair Loss Photos Alopecia and other Hair Diseases', 'Herpes HPV and other STDs Photos', 'Light Diseases and Disorders of Pigmentation', 'Lupus and other Connective Tissue diseases', 'Melanoma Skin Cancer Nevi and Moles', 'Nail Fungus and other Nail Disease', 'Poison Ivy Photos and other Contact Dermatitis', 'Psoriasis pictures Lichen Planus and related diseases', 'Scabies Lyme Disease and other Infestations and Bites', 'Seborrheic Keratoses and other Benign Tumors', 'Systemic Disease', 'Tinea Ringworm Candidiasis and other Fungal Infections', 'Urticaria Hives', 'Vascular Tumors', 'Vasculitis Photos', 'Warts Molluscum and other Viral Infections']
 
 app = Flask(__name__)
 
+# Load the trained model
+model = tf.keras.models.load_model("./model/image_classifier_model.h5")
+
+# Define class names
+class_names = [
+    'Acne and Rosacea Photos',
+    'Actinic Keratosis Basal Cell Carcinoma and other Malignant Lesions',
+    'Atopic Dermatitis Photos',
+    'Bullous Disease Photos',
+    'Cellulitis Impetigo and other Bacterial Infections',
+    'Eczema Photos',
+    'Exanthems and Drug Eruptions',
+    'Hair Loss Photos Alopecia and other Hair Diseases',
+    'Herpes HPV and other STDs Photos',
+    'Light Diseases and Disorders of Pigmentation',
+    'Lupus and other Connective Tissue diseases',
+    'Melanoma Skin Cancer Nevi and Moles',
+    'Nail Fungus and other Nail Disease',
+    'Poison Ivy Photos and other Contact Dermatitis',
+    'Psoriasis pictures Lichen Planus and related diseases',
+    'Scabies Lyme Disease and other Infestations and Bites',
+    'Seborrheic Keratoses and other Benign Tumors',
+    'Systemic Disease',
+    'Tinea Ringworm Candidiasis and other Fungal Infections',
+    'Urticaria Hives',
+    'Vascular Tumors',
+    'Vasculitis Photos',
+    'Warts Molluscum and other Viral Infections'
+]
+
+# Image preprocessing
 def preprocess_image(image):
-    image = image.resize((128, 128)) 
-    image = np.array(image) / 255.0  
-    image = np.expand_dims(image, axis=0) 
+    image = image.resize((224, 224))  # <-- Change this to 224x224
+    image = np.array(image) / 255.0
+    image = np.expand_dims(image, axis=0)
     return image
 
 @app.route('/predict', methods=['POST'])
 def predict():
     if 'file' not in request.files:
         return jsonify({'error': 'No file uploaded'}), 400
-
     file = request.files['file']
     image = Image.open(file)
     processed_image = preprocess_image(image)
@@ -33,6 +56,7 @@ def predict():
     confidence = float(np.max(predictions))
 
     return jsonify({'class': predicted_class, 'confidence': confidence})
+
 
 if __name__ == "__main__":
     app.run(debug=True)
